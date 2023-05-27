6d4687c73eb09baa070e2192336ece7b6d22835a
wilyCesde <wruizza@cesde.net>
2023-05-27 11:01:35 -0500

    pandas

--- main.py
+++ main.py
@@ -6,16 +6,22 @@ import pandas as pd
 tabla1=pd.DataFrame(apartamento1,columns=['edades'])
 tabla2=pd.DataFrame(apartamento2,columns=['edades'])
 tabla3=pd.read_csv('./data/empleados.csv')
-#####################################################
-estadisticasAPTO1=tabla1.describe()
-estadisticasAPTO2=tabla2.describe()
-estadisticasEmpleados=tabla3.describe()
-#####################################################
-print(estadisticasAPTO1)
-print('\n')
-print(estadisticasAPTO2)
-print('\n')
-print(estadisticasEmpleados)
-#####################################################
+
+
+print(tabla3)
+
+
+
+# #####################################################
+# estadisticasAPTO1=tabla1.describe()
+# estadisticasAPTO2=tabla2.describe()
+# estadisticasEmpleados=tabla3.describe()
+# #####################################################
+# print(estadisticasAPTO1)
+# print('\n')
+# print(estadisticasAPTO2)
+# print('\n')
+# print(estadisticasEmpleados)
+# #####################################################
 
 
